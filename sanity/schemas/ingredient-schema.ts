8f68cf07921c86bf94f34a38f42fb75ab7501e6b
Hunter Brown <hunterstokes@me.com>
2023-05-11 00:07:56 -0700

    tweaking the sanity schemas for better client access

---
+++ sanity/schemas/ingredient-schema.ts
@@ -0,0 +1,15 @@
+const ingredient = {
+    name: 'ingredient',
+    title: 'Ingredient',
+    type: 'document',
+    fields: [{
+        title: 'Name',
+        name: 'name',  
+        type: 'string',
+    }, {
+        title: 'Description',
+        name: 'description',
+        type: 'string',
+    }
+],
+}
--- sanity/schemas/recipe-schema.ts
+++ sanity/schemas/recipe-schema.ts
@@ -1,26 +1,82 @@
-
-const recipe = { 
+const recipe = {
+    
     name: 'recipe',
     title: 'Recipe',
     type: 'document',
-    fields: [ {
-        name: 'title',
-        title: 'Title',
-        type: 'string',
-}, {
-    name: 'slug',
-    title: 'Slug',
-    type: 'slug', 
-    options: {source: 'name'}
-}, {
-    name: 'author',
-    title: 'Author',
-    type: 'string',
-},{
-    name: 'mediaSource',
-    title: 'Media Source',
-    type: 'string',
-}],
-};
+    fields: [
+        {
+            name: 'name',
+            title: 'Recipe Name',
+            type: 'string',
+        },{
+            name: 'description',
+            title: 'Description',
+            type: 'text',
+        }, {
+            name: 'ingredients',
+            title: 'Ingredients',
+            type: 'array',
+            of: [{
+                type: 'object',
+                fields: [{
+                    name: 'ingredient',
+                    title: 'Ingredient',
+                    type: 'reference',
+                    to: [{type: 'ingredient'}],
+        },{
+            name: 'amount',
+            title: 'Amount',
+            type: 'number',
+        }, {
+            name: 'fraction',
+            title: 'Fraction',
+            type: 'string',
+            options: {
+                list: ['1/2', '1/3', '1/4', '2/3', '3/4', '']
+        }},{
+            name: 'unit',
+            title: 'Unit',
+            type: 'string',
+            options: {
+                list: ['cup', 'ounce', 'tsp', 'tbsp','quart', 'pint', 'gallon', 'lb', 'gram', 'kg', 'ml', 'l'],
+                // function() {
+                //     if (this.amount > 1) {
+                //         return this.list + 's';
+                //     }
+                // }
 
+        },}
+    ],
+    preview: {
+        select: {
+            title: 'ingredient.name',
+            name: 'ingredient.name',
+            amount: 'amount',
+            fraction: 'fraction',
+            unit: 'unit',
+
+        }, prepare({
+            title,
+            name,
+            amount,
+            fraction = 'No Fraction Set',
+            unit = 'No Unit Set',
+            
+        } : {title?: string, name?: string, amount?: number, fraction?: string, unit?: string}) {
+            return {
+                title: title,
+                name: name,
+                subtitle: `${amount} ${fraction} ${unit}`,
+            }
+        }
+    }
+        }
+    ]},{
+        name: 'instructions',
+        title: 'Instructions',
+        type: 'array',
+        of: [{ type: 'block' }]
+    }
+]
+} 
 export default recipe;
