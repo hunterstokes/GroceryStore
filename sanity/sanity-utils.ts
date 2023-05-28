830c87cc4e9a8d765e3ca6b86e8fd4e6a1d18561
[Hunterstokes] <hunterstokes@me.com>
2023-05-28 09:29:34 -0700

    setting up access to the sanity data

---
+++ sanity/sanity-utils.ts
@@ -0,0 +1,18 @@
+import { createClient, groq } from "next-sanity";
+
+export async function getRecipe() {
+    const client = createClient({
+        projectId: "r6nc7qm6",
+        dataset: "production",
+        apiVersion: "2023-05-09",
+
+});
+
+ client.fetch(
+    groq`*[_type == "recipe"]{
+        _id,
+        _createdAt,
+        name,
+        description,
+
+}`)}
