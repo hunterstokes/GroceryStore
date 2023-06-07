7e7b1dc4ff23c80bec3a0aa14da2cf3692ddf473
Hunter Brown <hunterstokes@me.com>
2023-06-07 11:24:45 -0700

    styling blog cards

--- app/blog/kitchentable/page.tsx
+++ app/blog/kitchentable/page.tsx
@@ -7,27 +7,25 @@ export default async function fakeBlog() {
 
   const blogPost = await getAltSchema();
 
-  console.log(blogPost);
-
 
   return (
     <main className="min-h-screen">
       <section>
         <Navbar />
-        <h1>BlogBS</h1>
+        <h1 className="text-6xl text-center m-3">Kitchen Table</h1>
 
-        <div className="m-autom p-5">
+        <div className="m-autom m-5">
           {blogPost.map((post: any) => (
-            <div className="p-5" key={post._id}>
-              <h2 className="text-2xl font-bold">{post.name}</h2>
-              <p>{post.content}</p>
+            <div className="m-5 p-2 bg-black box-content border-2 rounded-xl" key={post._id}>
+              <h2 className="text-3xl font-bold">{post.name}</h2>
+              <p className="font-thin">{post.content}</p>
               <img src={post.image?.asset?.url} alt={post.image?.alt} />
               <p>Recipe: {post.recipe}</p>
               <h2>Ingredients:</h2>
-              <ul>
+              <ul className=" italic">
                 {Array.isArray(post.ingredients) ? (
                   post.ingredients.map((ingredient: string, index: number) => (
-                    <li key={index}>{ingredient}</li>
+                    <li key={index}>- {ingredient}</li>
                   ))
                 ) : (
                   <li>No ingredients available</li>
